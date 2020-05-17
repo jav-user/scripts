63d1dd5cdca3ac6496abebde2b114b6678f8aa97
jav-user <62957911+jav-user@users.noreply.github.com>
2020-05-17 14:45:03 -0500

    Update thumbs.js

--- rarbg/thumbs.js
+++ rarbg/thumbs.js
@@ -1,11 +1,18 @@
 //rarbg.com thumbnails
+document.querySelectorAll(".lista2t a[title][onmouseover]").forEach((v) => {
+    if (v.onmouseover) {
 
-document.querySelectorAll("a[title][onmouseover]").forEach((v)=>{
-if(v.onmouseover){
- var omo=v.onmouseover.toString()
- var url=omo.split("'")[2].split("\\")[0]
- v.parentNode.parentNode.querySelector("img").src=url
- //v.onmouseover=null
-}
-
+        var omo = v.onmouseover.toString()
+        var url = omo.split("'")[2].split("\\")[0]
+        v.parentNode.parentNode.querySelector("img").src = url
+            //   console.log("url",url)
+        var poster = url.split("/")
+        poster[3] = 'posters2'
+        poster[4] = poster[5] ? poster[5].substr(0, 1) : ""
+        poster = poster.join("/")
+        v.onmouseover = function() {
+            return overlib(`<img src="${poster}" border=0/>`)
+        }
+    }
 })
+
