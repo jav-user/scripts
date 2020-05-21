9e3c7ec61ad7cb368577820f9cf0baf032d0432c
jav-user <62957911+jav-user@users.noreply.github.com>
2020-05-20 23:16:42 -0500

    Update torrent.js

--- rarbg/torrent.js
+++ rarbg/torrent.js
@@ -1,46 +1,48 @@
-var imgPlugins = {}
-var batch = db.batch()
-    // var missingPlugins = {}
+if (window.location.pathname.startsWith("/torrent/")) {
+    
+    var imgPlugins = {}
+    var batch = db.batch()
 
-Rarbg
-    .collection("imgPlugins")
-    .get()
-    .then(q => {
-        q.forEach(doc => {
-            imgPlugins[doc.id] = function(imgSrc, link) {
-                var fn = doc.data().f
-                console.log(doc.id, fn)
-                return eval(fn)
-            }
-        })
-
-        document.querySelectorAll("#description img").forEach(img => {
-            var dominio = img.src.split("/")[2]
-            var link = img.parentNode.href
-            console.log("imgSrc", img.src)
-            var id = dominio
-            var newSrc = imgPlugins[id] ? imgPlugins[id](img.src, link) : img.src
-            if (img.src != newSrc) {
-                console.log("newSrc: " + newSrc)
-                img.src = newSrc
-                img.parentNode.href = newSrc
-            } else {
-                var ref = Rarbg.collection("missingPlugins").doc(id)
-                var doc = {
-                    page: window.location.href,
-                    imgSrc: img.src,
-                    link: link,
-                    date: Date.now(),
-                    dateStr: new Date().toString()
+    Rarbg
+        .collection("imgPlugins")
+        .get()
+        .then(q => {
+            q.forEach(doc => {
+                imgPlugins[doc.id] = function(imgSrc, link) {
+                    var fn = doc.data().f
+                    console.log(doc.id, fn)
+                    return eval(fn)
                 }
+            })
 
-                batch.set(ref, doc)
-            }
+            document.querySelectorAll("#description img").forEach(img => {
+                var dominio = img.src.split("/")[2]
+                var link = img.parentNode.href
+                console.log("imgSrc", img.src)
+                var id = dominio
+                var newSrc = imgPlugins[id] ? imgPlugins[id](img.src, link) : img.src
+                if (img.src != newSrc) {
+                    console.log("newSrc: " + newSrc)
+                    img.src = newSrc
+                    img.parentNode.href = newSrc
+                } else {
+                    var ref = Rarbg.collection("missingPlugins").doc(id)
+                    var doc = {
+                        page: window.location.href,
+                        imgSrc: img.src,
+                        link: link,
+                        date: Date.now(),
+                        dateStr: new Date().toString()
+                    }
+
+                    batch.set(ref, doc)
+                }
+            })
+            batch.commit()
         })
+}
 
-        batch.commit()
 
-    })
 
 /*
 var rarbgImg={
