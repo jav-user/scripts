fee505a8ba1b23817031e57c3b8c4501994c8814
jav-user <62957911+jav-user@users.noreply.github.com>
2020-05-20 23:33:04 -0500

    Update torrent.js

--- rarbg/torrent.js
+++ rarbg/torrent.js
@@ -42,70 +42,11 @@ if (window.location.pathname.startsWith("/torrent/")) {
         })
 
 
-    var tds = Array.from(document.querySelectorAll("table tr td.lista")).map(td => td.innerText.trim()).filter(txt => txt.endsWith("MB") || txt.endsWith("GB"))
+    var tds = Array
+                .from(document.querySelectorAll("table tr td.lista"))
+                .map(td => td.innerText.trim())
+                .filter(txt => txt.endsWith("MB") || txt.endsWith("GB"))
     var h1 = document.querySelector("h1")
     h1.innerText = h1.innerText + ` [${tds[0]}]`
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-if (window.location.pathname.startsWith("/torrents.php")) {
-
-    document.querySelectorAll(".lista2t a[title][onmouseover]").forEach((v) => {
-        if (v.onmouseover) {
-
-            var omo = v.onmouseover.toString()
-            var url = omo.split("'")[2].split("\\")[0]
-            var td = v.parentNode.parentNode.querySelector("td")
-            td.align = "center"
-            var img = v.parentNode.parentNode.querySelector("img")
-            img.src = url
-                //   console.log("url",url)
-            var poster = url.split("/")
-            poster[3] = 'posters2'
-            poster[4] = poster[5] ? poster[5].substr(0, 1) : ""
-            poster = poster.join("/")
-            v.onmouseover = function() {
-                return overlib(`<img style="max-height:500px" src="${poster}" border=0 onerror="this.src='${url}'"/>`)
-            }
-        }
-    })
-
-
-    var img = document.createElement("img")
-    img.src = "https://i.imgur.com/RpqqCgl.jpg"
-
-    for (var i = 0; i < 3; i++) {
-        document.querySelector("body").append(img)
-    }
-
-}
-
-
-
-
-
-
-/*
-var rarbgImg={
-    "22pixx.xyz": src => src.replace("/s/","/i/")
-                            .replace("/os/","/o/"),
-    "imagecurl.com": src => src.replace("_thumb",""),
-}
-*/
