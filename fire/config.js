e2ab46ac4550371465e90728f6b9c79fd1cf579b
jav-user <62957911+jav-user@users.noreply.github.com>
2020-05-20 20:34:06 -0500

    Create config.js

---
+++ fire/config.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyA8st5ho2vz3OKWgIBeCTtJn6w-z_njOJ4",
+    authDomain: "jav-user.firebaseapp.com",
+    databaseURL: "https://jav-user.firebaseio.com",
+    projectId: "jav-user",
+    storageBucket: "jav-user.appspot.com",
+    messagingSenderId: "500259977827",
+    appId: "1:500259977827:web:0a2973b90fb0bcf1a5d656",
+    measurementId: "G-498QT6E8S5"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  //firebase.analytics();
