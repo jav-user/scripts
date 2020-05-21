c9d284e26a4c62dfba9f609aa5e848393a000fc3
jav-user <62957911+jav-user@users.noreply.github.com>
2020-05-20 19:33:14 -0500

    Update desc.js

--- rarbg/desc.js
+++ rarbg/desc.js
@@ -1,11 +1,14 @@
 const rarbgImg={
-    "22pixx.xyz": url => url.replace("/s/","/i/")
+    "22pixx.xyz": src => src.replace("/s/","/i/")
 }
 
 $("#description img").each((i,img)=>{
     var dominio = img.src.split("/")[2]
+    var link = img.parentNode.href
     console.log(img.src)
-    var src = rarbgImg[dominio] ? rarbgImg[dominio](img.src) : img.src 
-    img.src=src
-    img.parentNode.href=src
+    var src = rarbgImg[dominio] ? rarbgImg[dominio](img.src, link) : img.src 
+    if(img.src!=src){
+        img.src=src
+        img.parentNode.href=src
+    }
 })
