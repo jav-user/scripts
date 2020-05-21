92ce7a8e6aec22062f0905481c026f571b9df3c5
jav-user <62957911+jav-user@users.noreply.github.com>
2020-05-20 21:51:57 -0500

    Update and rename scriptnav.js to _script.js

---
+++ rarbg/_script.js
@@ -0,0 +1,13 @@
+// AceScript (chrome)
+
+// ==UserScript==
+// @name Rarbg
+// @namespace Rarbg
+// @match {{*://rarbgweb.org/*}}
+// @grant none
+// @require https://www.gstatic.com/firebasejs/7.14.4/firebase-app.js
+// @require https://www.gstatic.com/firebasejs/7.14.4/firebase-firestore.js
+// @require https://jav-user.github.io/scripts/fire/config.js
+// @require https://jav-user.github.io/scripts/rarbg/thumbs.js
+// @require https://jav-user.github.io/scripts/rarbg/desc.js
+// ==/UserScript==
