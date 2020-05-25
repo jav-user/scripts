08b93a5c51ce5c856c4798cef139fdce89084254
jav-user <62957911+jav-user@users.noreply.github.com>
2020-05-25 18:17:00 -0500

    Update torrents.js

--- rarbg/torrents.js
+++ rarbg/torrents.js
@@ -28,18 +28,35 @@ if (window.location.pathname.startsWith("/torrents.php")) {
         document.querySelector("body").append(img)
     }
     
+
+    var seasons = function(){
+        var ss=[]
+        for(var i =1; i<=99;i++){
+            var num = "000000"+i;
+            var season ="S"+num.substr(-2)
+            ss.push(season)
+        }
+        return ss
+    }
+    
     Array.from(document.querySelectorAll(".lista2t a")).filter(a=>a.href.includes("/torrent/"))
         .forEach(a=>{
+        
+            //var arr 
+            
+            seasons().forEach(season=>{
+                if(a.innerText.includes("."+season+".")){
+                   a.style.color="purple"
+                   a.style.fontSize="12px"
+                }
+            })
+            
             if(a.innerText.includes("ION10")) {
                 a.style.color="green"
                 a.style.fontSize="12px"
-            }else if ([
-                    "S01","S02","S03","S04","S05","S06","S07","S08","S09","S10",
-                    "S11","S12","S13","S14","S15","S16","S17","S18","S19","S20"
-                ].indexOf(a.innerText.split("."))!=-1){
-                a.style.color="purple"
-                a.style.fontSize="12px"
             }
+                  //  "S01","S02","S03","S04","S05","S06","S07","S08","S09","S10",
+                    //"S11","S12","S13","S14","S15","S16","S17","S18","S19","S20"
 
       })
 }
