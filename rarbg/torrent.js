d5c0f86ab6471a49121bca27dfb49296e14989e2
jav-user <62957911+jav-user@users.noreply.github.com>
2020-05-29 13:49:35 -0500

    Update torrent.js

--- rarbg/torrent.js
+++ rarbg/torrent.js
@@ -162,6 +162,7 @@ if (window.location.pathname.startsWith("/torrent/")) {
             Rarbg.collection("Torrents").doc(`${TorrentName} [${TorrentSize}]`).set(torrent)
                 .then(r => {
                     clicked = true
+                    mgt.parent.style.color="green"
                     console.log("clicked")
                 })
 
