9aceeaf71bc1c6fdac4d9931e8b08aae2d6163a9
jav-user <62957911+jav-user@users.noreply.github.com>
2020-05-29 20:41:50 -0500

    Create nes_functions.js

---
+++ nes/nes_functions.js
@@ -0,0 +1,28 @@
+const _nes = function () {
+    var x = this;
+    Array.prototype.sortRandom = function () {
+        return this.sort(() => 0.5 - Math.random());
+    };
+
+    x.addScript = function (src, id) {
+        var jq = document.createElement("script");
+        jq.id = id;
+        jq.src = src + "?var=" + x.randomBetween(100, 1000);
+        var head = document.querySelector("head");
+        if (!head.querySelector(`script[src="${src}"]`)) head.appendChild(jq);
+    };
+
+    x.orderObj = function (obj) {
+        const ordered = {};
+        Object.keys(obj)
+            .sort()
+            .forEach((key) => (ordered[key] = obj[key]));
+        return ordered;
+    };
+
+    x.randomBetween = function (min, max) {
+        return Math.floor(Math.random() * (max - min + 1) + min);
+    };
+};
+
+const nes = new _nes()
