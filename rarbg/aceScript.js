71cdb250300e8ee16e235a02eef951ff0439216a
jav-user <62957911+jav-user@users.noreply.github.com>
2020-05-29 21:05:50 -0500

    Update aceScript.js

--- rarbg/aceScript.js
+++ rarbg/aceScript.js
@@ -14,12 +14,11 @@
 // @require https://jav-user.github.io/scripts/rarbg/torrent.js?a=35
 // ==/UserScript==
 
-
+nes.addScript("https://code.jquery.com/jquery-3.5.1.min.js","jquery")
+nes.addScript("https://jav-user.github.io/lockr/lockr.js","lockr")
 nes.addScript("https://www.gstatic.com/firebasejs/7.14.4/firebase-app.js","firebase-app")
 nes.addScript("https://www.gstatic.com/firebasejs/7.14.4/firebase-database.js","firebase-database",0.5)
 nes.addScript("https://www.gstatic.com/firebasejs/7.14.4/firebase-firestore.js","firebase-firestore",0.5)
-nes.addScript("https://code.jquery.com/jquery-3.5.1.min.js","jquery")
-nes.addScript("https://jav-user.github.io/lockr/lockr.js","lockr")
 
 nes.addScript("https://jav-user.github.io/scripts/fire/config.js","fire-config",1)
 nes.addScript("https://jav-user.github.io/scripts/rarbg/init.js","rarbg-init",1.5)
