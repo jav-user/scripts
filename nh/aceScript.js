90ffee90dacdfacf3c9ac54f92b04ad87d96406a
jav-user <62957911+jav-user@users.noreply.github.com>
2020-05-31 20:00:20 -0500

    Update aceScript.js

--- nh/aceScript.js
+++ nh/aceScript.js
@@ -7,7 +7,7 @@
 // @require https://jav-user.github.io/scripts/nh/aceScript.js?a=1
 // ==/UserScript==
 
-nes.addScript("https://code.jquery.com/jquery-3.5.1.min.js","jquery")
+// nes.addScript("https://code.jquery.com/jquery-3.5.1.min.js","jquery")
 nes.addScript("https://jav-user.github.io/lockr/lockr.js","lockr")
 nes.addScript("https://www.gstatic.com/firebasejs/7.14.4/firebase-app.js","firebase-app")
 nes.addScript("https://www.gstatic.com/firebasejs/7.14.4/firebase-database.js","firebase-database",0.5)
