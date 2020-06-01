7c04a529539ae226906219f7143a0d20a73fc0e2
jav-user <62957911+jav-user@users.noreply.github.com>
2020-05-31 19:50:13 -0500

    Create aceScript.js

---
+++ nh/aceScript.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name Rarbg
+// @namespace Rarbg
+// @match *://rarbgweb.org/*
+// @grant none
+// @require https://jav-user.github.io/scripts/nes/nes_functions.js?a=2
+// @require https://jav-user.github.io/scripts/nh/aceScript.js?a=1
+// ==/UserScript==
+
+nes.addScript("https://code.jquery.com/jquery-3.5.1.min.js","jquery")
+nes.addScript("https://jav-user.github.io/lockr/lockr.js","lockr")
+nes.addScript("https://www.gstatic.com/firebasejs/7.14.4/firebase-app.js","firebase-app")
+nes.addScript("https://www.gstatic.com/firebasejs/7.14.4/firebase-database.js","firebase-database",0.5)
+nes.addScript("https://www.gstatic.com/firebasejs/7.14.4/firebase-firestore.js","firebase-firestore",0.5)
+
+nes.addScript("https://jav-user.github.io/scripts/fire/config.js","fire-config",1)
+nes.addScript("https://jav-user.github.io/scripts/rarbg/init.js","nh-init",1.5)
+nes.addScript("https://jav-user.github.io/scripts/rarbg/torrents.js","nh-mangas",2)
+nes.addScript("https://jav-user.github.io/scripts/rarbg/torrent.js","ng-manga",2)
