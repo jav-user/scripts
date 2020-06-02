c28a0c43c8662f25b82e57236960385002f2f1f2
jav-user <62957911+jav-user@users.noreply.github.com>
2020-06-01 22:56:53 -0500

    Update nes_functions.js

--- nes/nes_functions.js
+++ nes/nes_functions.js
@@ -1,42 +1,72 @@
 const _nes = function () {
     var x = this;
+
+    Array.prototype.filterIfIncludes = function (q, isOr) {
+        var Q = Array.isArray(q) ? q : [q];
+        return this.filter((val) => {
+            var sw = !isOr;
+            Q.forEach((q) => {
+                let includes = (val + "").includes(q);
+                sw = isOr ? sw || includes : sw & includes;
+            });
+            return sw;
+        });
+    };
+
+    Array.prototype.includes = function (q, isOr) {
+        var vm = this;
+        var sw = !isOr;
+        var Q = Array.isArray(q) ? q : [q];
+        Q.forEach((q) => {
+            let includes = vm.some((val) => (val + "").includes(q));
+            sw = isOr ? sw || includes : sw & includes;
+        });
+
+        return sw;
+    };
+
     Array.prototype.sortRandom = function () {
         return this.sort(() => 0.5 - Math.random());
     };
-    
-    Array.prototype.unique = function(){
-        return this.filter((el,i,a)=>a.indexOf(el)==i)
-    }
-    
-    String.prototype.copy = function(){
-        var vm =this.toString()  
-        console.log("string... ", vm)
+
+    Array.prototype.unique = function () {
+        return this.filter((el, i, a) => a.indexOf(el) == i);
+    };
+
+    String.prototype.copy = function () {
+        var vm = this.toString();
+        console.log("string... ", vm);
         var tempInput = document.createElement("input");
         tempInput.value = vm;
         document.body.appendChild(tempInput);
         tempInput.select();
         document.execCommand("copy");
         document.body.removeChild(tempInput);
-        return true
-    }
-    x._get=function(){
-        var search = window.location.search.split("?")[1]
-        var data = {}
-        if(!search) return data
-        search.split("&").forEach(s=>{
-            var key = s.split("=")[0]
-            var value = s.split("=")[1]
-            data[key]=value
-        })
-        return data
-    }
+        return true;
+    };
+
+    x._get = function () {
+        var search = window.location.search.split("?")[1];
+        var data = {};
+        if (!search) return data;
+        search.split("&").forEach((s) => {
+            var key = s.split("=")[0];
+            var value = s.split("=")[1];
+            data[key] = value;
+        });
+        return data;
+    };
+
     x.addScript = function (src, id, delay) {
-        if(!delay) delay = 0
+        if (!delay) delay = 0;
         var jq = document.createElement("script");
         jq.id = id;
         jq.src = src + "?var=" + x.randomBetween(100, 1000);
         var head = document.querySelector("head");
-        if (!head.querySelector(`script[src="${src}"]`)) setTimeout(()=>{head.appendChild(jq)},delay*1000)
+        if (!head.querySelector(`script[src="${src}"]`))
+            setTimeout(() => {
+                head.appendChild(jq);
+            }, delay * 1000);
     };
 
     x.orderObj = function (obj) {
@@ -52,4 +82,4 @@ const _nes = function () {
     };
 };
 
-const nes = new _nes()
+const nes = new _nes();
