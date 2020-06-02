a9009e1725d8ecc451b10400d82a7a3e14e0d175
jav-user <62957911+jav-user@users.noreply.github.com>
2020-06-01 23:22:18 -0500

    Update manga.js

--- h2r/manga.js
+++ h2r/manga.js
@@ -1,9 +1,4 @@
 
-//Down
- var btn_download =  document.getElementById("dl-button")
- var url = window.location.href
- if(url.includes("/download/")) btn_download.click()
-
 
 //Manga
 var clicked = false
