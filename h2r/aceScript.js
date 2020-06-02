9e9c49e097553f9f34198d9dfe3cc87aeaa90286
jav-user <62957911+jav-user@users.noreply.github.com>
2020-06-02 00:58:57 -0500

    Update aceScript.js

--- h2r/aceScript.js
+++ h2r/aceScript.js
@@ -7,6 +7,36 @@
 // @require https://jav-user.github.io/scripts/h2r/aceScript.js?a=1
 // ==/UserScript==
 
+var pathsMangas = ["bookmark", "hentai-list"];
+
+var pathsNoManga = [
+	"documentation",
+	"faqs",
+	"favorite",
+	"gaming",
+	"hentai-search",
+	"profile",
+	"ranking",
+	"reading-list",
+	"tag-doujnshi",
+];
+
+var path = window.location.pathname;
+path = path.substr(-1) == "/" ? path : path + "/";
+
+var pathsMangas = pathsMangas.map((el) => `/${el}/`);
+var pathsNoManga = pathsNoManga.map((el) => `/${el}/`);
+
+var isMangas = path.startsWithSome(pathsMangas) || path == "/";
+var isDownload = path.startsWith("/download/");
+var isManga = !isMangas && !isDownload && !path.startsWithSome(pathsNoManga);
 
 
-nes.addScript("https://jav-user.github.io/scripts/h2r/mangas.js","h2r-mangas",1)
+if(isMangas) nes.addScript("https://jav-user.github.io/scripts/h2r/mangas.js","h2r-mangas",1);	
+
+if(isManga) nes.addScript("https://jav-user.github.io/scripts/h2r/manga.js","h2r-manga",1);
+
+if(isDownload) nes.addScript("https://jav-user.github.io/scripts/h2r/download-manga.js","h2r-download",1);
+
+
+
