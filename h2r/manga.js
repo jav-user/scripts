c41cd00c95aba3c9ddc12d39c410365bd38024a9
jav-user <62957911+jav-user@users.noreply.github.com>
2020-06-01 22:59:04 -0500

    Create manga.js

---
+++ h2r/manga.js
@@ -0,0 +1,62 @@
+
+//Down
+ var btn_download =  document.getElementById("dl-button")
+ var url = window.location.href
+ if(url.includes("/download/")) btn_download.click()
+
+
+//Manga
+var clicked = false
+
+var openDownloads = function(){
+    var btns_circle = document.querySelectorAll("a.btn-circle")
+
+    btns_circle.forEach((btn,i)=>{
+        btn.target="_blank"
+        setTimeout(()=>{
+            btn.click()
+        },(i+1)*1000)
+    })
+}
+
+
+var clickBookmark=function(){
+    var bookmark = document.querySelectorAll(".dropdown-menu .usrBookmark")[0]
+    if(bookmark) bookmark.click()
+}
+
+var lastFile = document.querySelectorAll(".nav-chapters .text-muted small")[0];
+if (lastFile) {
+    var lastDate = lastFile.innerText;
+    var dt = lastDate
+        .split("]")[1]
+        .replace("about", "")
+        .replace("ago", "")
+        .replace("s", "")
+        .replace("minute", "m")
+        .replace("hour", "h")
+        .replace("day", "d")
+        .replace("week", "W")
+        .replace("month", "M")
+        .replace("year", "Y")
+        .replace(/ /g, "");
+
+    var cpt = lastFile.parentNode.parentNode.innerText.split(" ")[0];
+
+    var h3 = Array.from(document.querySelectorAll(".block-title")).filter(
+        (h3) => h3.innerText.trim() == "DISCUSSION"
+    )[0];
+    var toCopy = `${cpt}:${dt}`;
+
+    h3.innerText = `DISCUSSION: ${toCopy}`;
+    var bookmark = document.querySelectorAll(".dropdown-menu .usrBookmark")[0]
+    bookmark.onclick = () => {
+        if(!clicked){
+             toCopy.copy();
+             //clickBookmarks()
+             openDownloads()
+             clicked = true
+        }
+       
+    };
+}
