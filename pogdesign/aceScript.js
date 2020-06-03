e18ff6fbf57261f245a38d81b33ad2ed3acb8df5
jav-user <62957911+jav-user@users.noreply.github.com>
2020-06-03 15:18:37 -0500

    Update aceScript.js

--- pogdesign/aceScript.js
+++ pogdesign/aceScript.js
@@ -2,4 +2,4 @@
 
 nes.addScript("https://jav-user.github.io/scripts/nes/nes_functions.js", "nes")
 nes.addScriptOnce(nes.cdn("jquery"),"jq")
-nes.addScript("https://jav-user.github.io/pogdesign/tv-calendar.js", "tv",1)
+nes.addScript("https://jav-user.github.io/scripts/pogdesign/tv-calendar.js", "tv",1)
