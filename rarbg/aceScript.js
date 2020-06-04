4999cf985622cde8375dddba70696acc94401e8a
jav-user <62957911+jav-user@users.noreply.github.com>
2020-06-04 10:51:33 -0500

    Update aceScript.js

--- rarbg/aceScript.js
+++ rarbg/aceScript.js
@@ -11,7 +11,7 @@ nes.addScript("https://code.jquery.com/jquery-3.5.1.min.js","jquery")
 nes.addScript("https://jav-user.github.io/lockr/lockr.js","lockr")
 nes.addScript("https://jav-user.github.io/scripts/nes/nes_functions.js","nes")
 nes.addScript("https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment.min.js","moment")
-nes.addScript("https://cdnjs.cloudflare.com/ajax/libs/moment-timezone/0.5.31/moment-timezone-with-data.min.js","moment-tz")
+nes.addScript("https://cdnjs.cloudflare.com/ajax/libs/moment-timezone/0.5.31/moment-timezone-with-data.min.js","moment-tz",.5)
 
 nes.addScript("https://www.gstatic.com/firebasejs/7.14.4/firebase-app.js","firebase-app")
 nes.addScript("https://www.gstatic.com/firebasejs/7.14.4/firebase-database.js","firebase-database",1)
