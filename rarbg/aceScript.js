5ae8a8f7a0666d722832e67264ac71c222a2f647
jav-user <62957911+jav-user@users.noreply.github.com>
2020-06-04 00:43:04 -0500

    Update aceScript.js

--- rarbg/aceScript.js
+++ rarbg/aceScript.js
@@ -9,6 +9,7 @@
 
 nes.addScript("https://code.jquery.com/jquery-3.5.1.min.js","jquery")
 nes.addScript("https://jav-user.github.io/lockr/lockr.js","lockr")
+nes.addScript("https://jav-user.github.io/scripts/nes/nes_functions.js","lockr")
 nes.addScript("https://www.gstatic.com/firebasejs/7.14.4/firebase-app.js","firebase-app")
 nes.addScript("https://www.gstatic.com/firebasejs/7.14.4/firebase-database.js","firebase-database",0.5)
 nes.addScript("https://www.gstatic.com/firebasejs/7.14.4/firebase-firestore.js","firebase-firestore",0.5)
