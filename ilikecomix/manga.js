95dc562f8c287fdc030b6e20545bbf71dd0b231f
jav-user <62957911+jav-user@users.noreply.github.com>
2020-06-06 13:13:29 -0500

    Update manga.js

--- ilikecomix/manga.js
+++ ilikecomix/manga.js
@@ -1,9 +1,9 @@
+//$("#btn_copy").remove()
 
-
-
-$("#btn_copy").remove()
 var $btn = $("<button id='btn_copy'>Copy All</button>")
+
 $(".post-inner.clearfix > h1").after($btn)
+
 $("#btn_copy").on("click",function(){
     var imgurls = Array.from($(".dgwt-jg-item.jg-entry.entry-visible > a")).map((a)=>(a.href))
     nesMg.getCmd(imgurls)
