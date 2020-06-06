62a95d0901725c42176108a9b060fc10146d595b
jav-user <62957911+jav-user@users.noreply.github.com>
2020-06-06 13:00:29 -0500

    Update mangas.js

--- nes/mangas.js
+++ nes/mangas.js
@@ -1,1 +1,31 @@
+var nesMangas = function (){
+CMD = ""
+NUM = 0
+var vm = this
+
+
+vm.getLines = function (imgurls, folder){
+
+    folder = folder ? folder : $("title").text().toValidFileName()
+
+    var lines = []
+    NUM = imgurls.length
+    imgurls.forEach((imgu,i)=>{
+        let num = `000000000${i+1}`.substr(-5)
+        let line = `downf "${imgu}=${num}" "${folder}" "${window.location.host}"`;
+        lines.push(line)
+    })
+    return lines.join("\n")
+}
+
+nm.getCmd = function(imgurls){
+    var lines = vm.getLines(imgurls)
+    var cmd = `D:\n${lines}\n`
+    CMD = cmd
+    return cmd
+}
+}
+
+
+
 
