60db9420b8f48f3bb99884ee99547a35b88e9ee5
jav-user <62957911+jav-user@users.noreply.github.com>
2020-06-05 23:49:49 -0500

    Update manga.js

--- manganelo/manga.js
+++ manganelo/manga.js
@@ -27,7 +27,7 @@ async function getLastMangaChapters(last) {
           console.log("imgs " + imgs.length);
           setTimeout((_) => {
             solve();
-          }, 2500);
+          }, 3*1000);
           //                 solve()
         })
         .catch((error) => {
