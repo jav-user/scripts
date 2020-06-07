0da73935d11cf343751c6dd2eb167e2111cc74b4
jav-user <62957911+jav-user@users.noreply.github.com>
2020-06-06 19:41:53 -0500

    Create mangas.js

---
+++ ilikecomix/mangas.js
@@ -0,0 +1,134 @@
+CMDS = {};
+
+var loadMangasGUI = function () {
+    var $custom_manga = $(`
+        <div class="nes_custom">
+            <div class="manga_info"></div>
+            <button class="btn_get">Get All</button>
+            
+            <center><button class="btn_copy" style="display:none">Copy</button></center>
+            <br/><br/>
+        </div>`);
+
+    $(".nes_custom").remove();
+
+    $(".post").each((i, post) => {
+        let $el = $custom_manga.clone();
+        let $h2 = $(post).find("h2");
+        let url = $h2.find("a").attr("href");
+        let folder = $h2.text().toValidFileName();
+        let $btn = $el.find("button.btn_get");
+        $btn.text("Get " + folder);
+        $btn.attr("data-folder", folder);
+        $btn.attr("data-url", url);
+        $btn.on("click", function () {
+            getManga($el);
+        });
+
+        let $btn_copy = $el.find("button.btn_copy");
+        $btn_copy.on("click", function () {
+            let cmd = CMDS[folder];
+            nes.copy(cmd.cmd);
+            alert("copied! " + cmd.num + " images");
+            console.log(cmd.cmd);
+            // let cmds = `D:\n${CMDS.join("\n")\n}`
+            // nes.copy(cmds)
+            // alert("Copied "+CMDS.length+" mangas!")
+            // console.log(cmds)
+        });
+
+        $h2.after($el);
+    });
+
+    var $custom_mangas = $(`
+        <div class="nes_custom">
+            <div class="mangas_info"></div>
+            <button class="btn_get_all">Get All</button>
+            <button class="btn_copy_all" style="display:none">Copy All</button>
+            <br/><br/>
+        </div>
+        `);
+
+    $custom_mangas.find("button.btn_get_all").on("click", getMangas);
+    $custom_mangas.find("button.btn_copy_all").on("click", copyMangas);
+
+    $("h1.page-title").after($custom_mangas);
+};
+
+loadMangasGUI();
+
+var getManga = function ($el, solve) {
+    var $btn = $el.find("button.btn_get");
+    var $btn_copy = $el.find("button.btn_copy");
+    var $info = $el.find(".manga_info");
+
+    var url = $btn.attr("data-url");
+    var folder = $btn.attr("data-folder");
+
+    $info.text("...loading");
+    $.get(url)
+        .then((html) => {
+            var $aimgs = $(html).find(".dgwt-jg-item > a");
+            var imgurls = Array.from($aimgs).map((a) => a.href);
+            // alert(imgurls.length);
+
+            //         console.log(imgurls, folder)
+            var cmd = nesMg.getCmd(imgurls, folder);
+            //         console.log(html)
+            // if (CMDS.indexOf(cmd)) CMDS.push(cmd);
+            CMDS[folder] = {
+                cmd: cmd,
+                num: imgurls.length,
+            };
+
+            $info.text(`Done! (${imgurls.length} images)`);
+            $btn.hide();
+            $btn_copy.show();
+            $("button.btn_copy_all").show();
+            if (solve) solve();
+
+            //         nes.copy(CMD)
+            // alert(NUM + " images copied!")
+        })
+        .catch((err) => {
+            alert("Error in " + folder + "!");
+            console.log("err", err);
+            if (solve) solve();
+        });
+};
+
+function copyMangas() {
+    // let cmds = CMDS.join("\n");
+    // nes.copy(cmds);
+    // alert("Copied " + CMDS.length + " mangas!");
+    // console.log(cmds);
+    //
+    let num = 0;
+    let cmds = [];
+    for (var key in CMDS) {
+        let cmd = CMDS[key];
+        cmds.push(cmd.cmd);
+        num = num + cmd.num;
+    }
+
+    let _cmds = cmds.join("\n");
+    nes.copy(_cmds);
+    console.log(_cmds);
+    alert(`Copied ${Object.keys(CMDS).length} mangas and ${num} images!`);
+}
+
+async function getMangas() {
+    var btns = Array.from($(".nes_custom .btn_get"));
+    // .slice(0, 3);
+    for (var [i, btn] of btns.entries()) {
+        await new Promise((solve) => {
+            var $el = $(btn).parent();
+            getManga($el, solve);
+            // console.log(btn);
+            var info = `Doing... ${$(btn).attr("data-folder")} (${i + 1} of ${
+                btns.length
+            })`;
+            $(".mangas_info").text(info);
+        });
+    }
+}
