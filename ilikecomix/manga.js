af274178a4dbfe6e28472b3bdfc24a1d009b0e69
jav-user <62957911+jav-user@users.noreply.github.com>
2020-06-06 22:47:31 -0500

    Update manga.js

--- ilikecomix/manga.js
+++ ilikecomix/manga.js
@@ -5,7 +5,7 @@ var $btn = $("<button id='btn_copy'>Copy All</button>")
 $(".post-inner.clearfix > h1").after($btn)
 
 $("#btn_copy").on("click",function(){
-    var imgurls = Array.from($(".dgwt-jg-item.jg-entry.entry-visible > a")).map((a)=>(a.href))
+    var imgurls = Array.from($(".dgwt-jg-item > a")).map((a)=>(a.href))
     nesMg.getCmd(imgurls)
     nes.copy(CMD)
     alert(NUM + " images copied!")
