96a9f2deb94c90b8beebf42fd5ffc3e639158e07
jav-user <62957911+jav-user@users.noreply.github.com>
2020-06-10 12:08:16 -0500

    Create addconfig.js

---
+++ fire/addconfig.js
@@ -0,0 +1,24 @@
+    var mask ="https://www.gstatic.com/firebasejs/7.14.4/firebase-app.js"
+    nes.addScriptOnce(nes.cdn("firebase-app"), "fb-app",1);
+    nes.addScriptOnce(nes.cdn("firebase-database"), "fb-db",1);
+    nes.addScriptOnce(nes.cdn("firebase-firestore"), "fb-store",1);
+
+
+    setTimeout(_)=>{
+    // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyA8st5ho2vz3OKWgIBeCTtJn6w-z_njOJ4",
+    authDomain: "jav-user.firebaseapp.com",
+    databaseURL: "https://jav-user.firebaseio.com",
+    projectId: "jav-user",
+    storageBucket: "jav-user.appspot.com",
+    messagingSenderId: "500259977827",
+    appId: "1:500259977827:web:0a2973b90fb0bcf1a5d656",
+    measurementId: "G-498QT6E8S5"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  const db = firebase.firestore()
+    
+    
+    },3*1000)
