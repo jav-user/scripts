1c50f22b0dcb4bccb35ead8d6b74dc9e127314f0
jav-user <62957911+jav-user@users.noreply.github.com>
2020-06-19 21:15:42 -0500

    Update images-down.js

--- images-down/images-down.js
+++ images-down/images-down.js
@@ -205,7 +205,7 @@ var getLines = function (imgs, folder) {
   folder = folder.replace("$pp$", pp);
   var lines = imgs.map((img, i) => {
     var num = ("00000000" + (i + 1)).substr(-5);
-    let line = `${img}=${num}`;
+    let line = `${img}==${num}`;
     return `nddown "${line}" "${folder}" "${window.location.hostname}"`;
   });
   return `D:\n${lines.join("\n")}\n`;
