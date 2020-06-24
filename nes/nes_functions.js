1915e0062ab00fdf64b991de4a9ad4684148dc8d
jav-user <62957911+jav-user@users.noreply.github.com>
2020-06-23 22:39:50 -0500

    Update nes_functions.js

--- nes/nes_functions.js
+++ nes/nes_functions.js
@@ -257,19 +257,19 @@ const _nes = function () {
         return getCdn();
     };
     
-    x.fileSizeRx = /^[0-9]{1,}[.]{0,1}[0-9]{0,}[ ]{0,}[kmg]{0,1}[i]{0,1}[b]$/
+    x.fileSizeRx = "[0-9]{1,}[.]{0,1}[0-9]{0,}[ ]{0,}[kmg]{0,1}[i]{0,1}[b]"
     
     x.getFileSize=function(str, _type){
         var str = str.trim().toLowerCase()
         str = str.replace("bytes","b")
 
-        var match = x.fileSizeRx
+        var match = "^"+x.fileSizeRx+"$"
         if(!str.match(match)) return false
 
-        var num = str.match(/[0-9]{1,}[.]{0,1}[0-9]{0,}/)[0]
+        var num = str.match(/^[0-9]{1,}[.]{0,1}[0-9]{0,}/)[0]
         num = Number.parseFloat(num)
 
-        var type = str.match(/[kmg]{0,1}[i]{0,1}[b]/)[0].replace("i","")
+        var type = str.match(/[kmg]{0,1}[i]{0,1}[b]$/)[0].replace("i","")
 
         var types ={
             b: 1,
