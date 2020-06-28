6621bf88bcbd14f2059fb027e56b2ec1114319cd
jav-user <62957911+jav-user@users.noreply.github.com>
2020-06-27 23:45:40 -0500

    Create require.js

---
+++ magnets/require.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name Magnets
+// @namespace AceScript Magnets
+// @require https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.0.0/crypto-js.min.js
+// @require https://jav-user.github.io/scripts/nes/nes_functions.js?a=4
+// @require https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js
+// @require https://code.jquery.com/ui/1.12.1/jquery-ui.min.js
+//// @require https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.16.0/jquery.validate.min.js
+// @require https://jav-user.github.io/jquery-longclick/jquery-longclick.js
+// @require https://www.jqueryscript.net/demo/Serialize-Form-To-Objects-jQuery-serializeObject/jquery.serializeObject.js
+// @require https://www.gstatic.com/firebasejs/7.14.4/firebase-app.js
+// @require https://www.gstatic.com/firebasejs/7.14.4/firebase-database.js
+// @require https://www.gstatic.com/firebasejs/7.14.4/firebase-firestore.js
+// @require https://jav-user.github.io/scripts/fire/config.js
+// @require https://jav-user.github.io/scripts/magnets/scripts.js
+// @require https://jav-user.github.io/scripts/magnets/magnets.js
+// @grant none
+// ==/UserScript==
+
+nes.addStyle(
+	"https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css",
+	"jq-ui-base"
+);
