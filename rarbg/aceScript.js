7faa3b789dfab559d7fc5e3f9fa45a9117eb7f54
jav-user <62957911+jav-user@users.noreply.github.com>
2020-07-01 00:53:12 -0500

    Update aceScript.js

--- rarbg/aceScript.js
+++ rarbg/aceScript.js
@@ -3,23 +3,17 @@
 // @namespace Rarbg
 // @match *://rarbgweb.org/*
 // @grant none
-// @require https://jav-user.github.io/scripts/nes/nes_functions.js?a=2
-// @require https://jav-user.github.io/scripts/rarbg/aceScript.js?a=1
+// @require https://jav-user.github.io/scripts/nes/nes_functions.js
+// @require https://www.gstatic.com/firebasejs/7.14.4/firebase-app.js
+// @require https://www.gstatic.com/firebasejs/7.14.4/firebase-database.js
+// @require https://www.gstatic.com/firebasejs/7.14.4/firebase-firestore.js
+// @require https://code.jquery.com/jquery-3.5.1.min.js
+// @require https://jav-user.github.io/lockr/lockr.js
+// @require https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment.min.js
+// @require https://cdnjs.cloudflare.com/ajax/libs/moment-timezone/0.5.13/moment-timezone-with-data.min.js
+// @require https://jav-user.github.io/scripts/fire/config.js
+// @require https://jav-user.github.io/scripts/rarbg/torrents.js
+// @require https://jav-user.github.io/scripts/rarbg/torrent.js?a=1
 // ==/UserScript==
-
-nes.addScript("https://code.jquery.com/jquery-3.5.1.min.js","jquery")
-nes.addScript("https://jav-user.github.io/lockr/lockr.js","lockr")
-nes.addScript("https://jav-user.github.io/scripts/nes/nes_functions.js","nes")
-nes.addScript("https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment.min.js","moment")
-nes.addScript("https://cdnjs.cloudflare.com/ajax/libs/moment-timezone/0.5.13/moment-timezone-with-data.min.js","moment-tz",.5)
-
-nes.addScript("https://www.gstatic.com/firebasejs/7.14.4/firebase-app.js","firebase-app")
-nes.addScript("https://www.gstatic.com/firebasejs/7.14.4/firebase-database.js","firebase-database",1)
-nes.addScript("https://www.gstatic.com/firebasejs/7.14.4/firebase-firestore.js","firebase-firestore",1)
-
-nes.addScript("https://jav-user.github.io/scripts/fire/config.js","fire-config",1.5)
-nes.addScript("https://jav-user.github.io/scripts/rarbg/init.js","rarbg-init",1.75)
-nes.addScript("https://jav-user.github.io/scripts/rarbg/torrents.js","rarbg-torrents",2)
-nes.addScript("https://jav-user.github.io/scripts/rarbg/torrent.js","rarbg-torrent",2)
 
 
